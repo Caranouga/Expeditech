c7cf294411a3dd9df7720fbda4a12101842cfd25
Caranouga <malonepayet@gmail.com>
2025-05-23 13:24:38 +0200

    Update tooltips for coal generator and simplify color handling in ModMaterialDataProvider (but it still doesn't work...)

--- src/main/java/fr/caranouga/expeditech/datagen/providers/compat/tinker/ModMaterialDataProvider.java
+++ src/main/java/fr/caranouga/expeditech/datagen/providers/compat/tinker/ModMaterialDataProvider.java
@@ -10,14 +10,6 @@ public class ModMaterialDataProvider extends MaterialDataProvider {
 
     @Override
     protected void addMaterials() {
-        addMaterial(TinkerCompatMaterials.caranite, 2, ORDER_GENERAL, false, getColor("E20000"));
-    }
-
-    private int getColor(String hex){
-        return Integer.parseInt(hex.replace("#", ""), 16);
-    }
-
-    private int getColor(int r, int g, int b){
-        return (r << 16) | (g << 8) | b;
+        addMaterial(TinkerCompatMaterials.caranite, 2, ORDER_GENERAL, false, 0xE20000);
     }
 }
