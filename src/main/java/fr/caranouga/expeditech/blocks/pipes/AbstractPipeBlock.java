6c982e1a3d7047a1b25d8fe57fa21fe80e1d9f71
Caranouga <malonepayet@gmail.com>
2025-05-25 19:16:30 +0200

    Implement waterlogging support for pipes and add VoxelUtils for shape combination

--- src/main/java/fr/caranouga/expeditech/blocks/pipes/AbstractPipeBlock.java
+++ src/main/java/fr/caranouga/expeditech/blocks/pipes/AbstractPipeBlock.java
@@ -1,11 +1,14 @@
 package fr.caranouga.expeditech.blocks.pipes;
 
+import fr.caranouga.expeditech.utils.VoxelUtils;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.PushReaction;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.Direction;
@@ -17,7 +20,7 @@ import net.minecraft.world.IWorld;
 
 import javax.annotation.Nullable;
 
-public abstract class AbstractPipeBlock extends Block {
+public abstract class AbstractPipeBlock extends Block implements IWaterLoggable {
     // Patch le fait que les pipes soient des blocs solides (ex: on voit a travers eux)
 
     public static final BooleanProperty NORTH = BooleanProperty.create("north");
@@ -26,6 +29,7 @@ public abstract class AbstractPipeBlock extends Block {
     public static final BooleanProperty WEST  = BooleanProperty.create("west");
     public static final BooleanProperty UP    = BooleanProperty.create("up");
     public static final BooleanProperty DOWN  = BooleanProperty.create("down");
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     public AbstractPipeBlock() {
         /*super(AbstractBlock.Properties.of(Material.METAL)
@@ -40,13 +44,15 @@ public abstract class AbstractPipeBlock extends Block {
                 .setValue(EAST, false)
                 .setValue(WEST, false)
                 .setValue(UP, false)
-                .setValue(DOWN, false));
+                .setValue(DOWN, false)
+                .setValue(WATERLOGGED, false)
+        );
     }
 
     // region BlockState
     @Override
     protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(NORTH, SOUTH, EAST, WEST, UP, DOWN);
+        pBuilder.add(NORTH, SOUTH, EAST, WEST, UP, DOWN, WATERLOGGED);
     }
 
     @Nullable
@@ -70,6 +76,10 @@ public abstract class AbstractPipeBlock extends Block {
 
     @Override
     public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, IWorld pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
+        if(pState.getValue(WATERLOGGED)){
+            pLevel.getLiquidTicks().scheduleTick(pCurrentPos, Fluids.WATER, Fluids.WATER.getTickDelay(pLevel));
+        }
+
         boolean canConnect = canConnectTo(pLevel, pFacingPos, pFacing);
         return pState.setValue(getProperty(pFacing), canConnect);
     }
@@ -90,18 +100,74 @@ public abstract class AbstractPipeBlock extends Block {
         return PushReaction.BLOCK;
     }
 
-    private static final VoxelShape SHAPE_CORE = Block.box(6.5, 6.5, 6.5, 9.5, 9.5, 9.5);
-    private static final VoxelShape SHAPE_NORTH = Block.box(6.5, 6.5, 5.5, 8.5, 8.5, 0);
-
-
-    @Override
-    public VoxelShape getShape(BlockState pState, IBlockReader pLevel, BlockPos pPos, ISelectionContext pContext) {
-        return super.getShape(pState, pLevel, pPos, pContext);
+    /*@Override
+    public VoxelShape getOcclusionShape(BlockState state, IBlockReader worldIn, BlockPos pos) {
+        return getShape(state);
     }
 
     @Override
-    public BlockRenderType getRenderShape(BlockState pState) {
-        return BlockRenderType.MODEL;
+    public VoxelShape getVisualShape(BlockState state, IBlockReader reader, BlockPos pos, ISelectionContext context) {
+        return getShape(state);
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return getShape(state);
+    }
+
+    @Override
+    public VoxelShape getBlockSupportShape(BlockState state, IBlockReader reader, BlockPos pos) {
+        return getShape(state);
+    }
+
+    @Override
+    public VoxelShape getInteractionShape(BlockState state, IBlockReader worldIn, BlockPos pos) {
+        return getShape(state);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }*/
+
+    @Override
+    public VoxelShape getShape(BlockState pState, IBlockReader pLevel, BlockPos pPos, ISelectionContext pContext) {
+        return getShape(pState);
+    }
+
+
+
+    private static final VoxelShape SHAPE_CORE = Block.box(6.5, 6.5, 6.5, 9.5, 9.5, 9.5);
+    private static final VoxelShape SHAPE_NORTH = Block.box(6.5, 6.5, 5.5, 8.5, 8.5, 0);
+    private static final VoxelShape SHAPE_EAST = Block.box(9.5, 6.5, 8.5, 10.5, 7.5, 9.5);
+    private static final VoxelShape SHAPE_SOUTH = Block.box(6.5, 6.5, 9.5, 9.5, 9.5, 16);
+    private static final VoxelShape SHAPE_WEST = Block.box(0, 6.5, 6.5, 6.5, 9.5, 9.5);
+    private static final VoxelShape SHAPE_UP = Block.box(6.5, 9.5, 6.5, 9.5, 16, 9.5);
+    private static final VoxelShape SHAPE_DOWN = Block.box(6.5, 0, 6.5, 9.5, 6.5, 9.5);
+
+    private VoxelShape getShape(BlockState state) {
+        VoxelShape shape = SHAPE_CORE;
+
+        if (state.getValue(UP)) {
+            shape = VoxelUtils.combine(shape, SHAPE_UP);
+        }
+        if (state.getValue(DOWN)) {
+            shape = VoxelUtils.combine(shape, SHAPE_DOWN);
+        }
+        if (state.getValue(SOUTH)) {
+            shape = VoxelUtils.combine(shape, SHAPE_SOUTH);
+        }
+        if (state.getValue(NORTH)) {
+            shape = VoxelUtils.combine(shape, SHAPE_NORTH);
+        }
+        if (state.getValue(EAST)) {
+            shape = VoxelUtils.combine(shape, SHAPE_EAST);
+        }
+        if (state.getValue(WEST)) {
+            shape = VoxelUtils.combine(shape, SHAPE_WEST);
+        }
+
+        return shape;
     }
 
     private boolean canConnectTo(IWorld world, BlockPos pos, Direction direction) {
---
+++ src/main/java/fr/caranouga/expeditech/utils/VoxelUtils.java
@@ -0,0 +1,14 @@
+package fr.caranouga.expeditech.utils;
+
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+
+public class VoxelUtils {
+    public static VoxelShape combine(VoxelShape... shapes) {
+        VoxelShape combined = VoxelShapes.empty();
+        for (VoxelShape shape : shapes) {
+            combined = VoxelShapes.or(combined, shape);
+        }
+        return combined;
+    }
+}
