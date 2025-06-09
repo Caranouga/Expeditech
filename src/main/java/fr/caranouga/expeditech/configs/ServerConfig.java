4f8b51a87076a0064d19d759b46bafe94a982754
Caranouga <malonepayet@gmail.com>
2025-06-09 23:35:16 +0200

    Added a message when a machine break

--- src/main/java/fr/caranouga/expeditech/configs/ServerConfig.java
+++ src/main/java/fr/caranouga/expeditech/configs/ServerConfig.java
@@ -8,7 +8,8 @@ public class ServerConfig {
 
     public static final ForgeConfigSpec.ConfigValue<Integer> exampleInteger;
     public static final ForgeConfigSpec.ConfigValue<Boolean> exampleBoolean;
-    public static final ForgeConfigSpec.ConfigValue<Boolean> machineDurabilitySwitch;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> machineDurability;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> broadcastMachineDurability;
 
     static {
         BUILDER.push("Expeditech Server Config");
@@ -20,9 +21,13 @@ public class ServerConfig {
                 .comment("An example boolean config value.")
                 .define("exampleBoolean", true);
 
-        machineDurabilitySwitch = BUILDER
+        machineDurability = BUILDER
                 .comment("Enable or disable machine durability.")
-                .define("machineDurabilitySwitch", true);
+                .define("machineDurability", true);
+
+        broadcastMachineDurability = BUILDER
+                .comment("Enable or disable broadcasting machine breakage to players.")
+                .define("broadcastMachineDurability", true);
 
         BUILDER.pop();
         SERVER_CONFIG = BUILDER.build();
--- src/main/java/fr/caranouga/expeditech/tiles/ETTileEntity.java
+++ src/main/java/fr/caranouga/expeditech/tiles/ETTileEntity.java
@@ -1,5 +1,7 @@
 package fr.caranouga.expeditech.tiles;
 
+import fr.caranouga.expeditech.Expeditech;
+import fr.caranouga.expeditech.configs.ServerConfig;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.nbt.CompoundNBT;
@@ -7,6 +9,9 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.Util;
+import net.minecraft.util.text.ChatType;
+import net.minecraft.util.text.TranslationTextComponent;
 
 public abstract class ETTileEntity extends TileEntity {
     private int maxUses;
@@ -38,11 +43,20 @@ public abstract class ETTileEntity extends TileEntity {
     protected void use(){
         maxUses--;
 
-        if(maxUses <= 0 && level != null && !level.isClientSide) {
+        if(maxUses <= 0 && level != null && !level.isClientSide && ServerConfig.machineDurability.get()) {
             BlockState state = level.getBlockState(getBlockPos());
             Block block = state.getBlock();
 
             this.level.playSound(null, getBlockPos(), SoundEvents.ANVIL_LAND, SoundCategory.BLOCKS, 1f, 1f);
+
+            if(ServerConfig.broadcastMachineDurability.get()){
+                level.getServer().getPlayerList().broadcastMessage(
+                        new TranslationTextComponent(Expeditech.MODID + ".machine_broken",  block.getName(), getBlockPos().getX(), getBlockPos().getY(), getBlockPos().getZ()),
+                        ChatType.CHAT,
+                        Util.NIL_UUID
+                );
+            }
+
             block.onRemove(state, level, getBlockPos(), state, false);
             this.level.removeBlock(getBlockPos(), false);
         }
