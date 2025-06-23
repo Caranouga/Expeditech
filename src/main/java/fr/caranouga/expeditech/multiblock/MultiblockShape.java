76602f58b6ac54bc89b8fa2640a35e386a2a9c9f
Caranouga <malonepayet@gmail.com>
2025-06-23 03:10:16 +0200

    Removed unused StringTextComponent import from MultiblockShape.java

--- src/main/java/fr/caranouga/expeditech/multiblock/MultiblockShape.java
+++ src/main/java/fr/caranouga/expeditech/multiblock/MultiblockShape.java
@@ -5,7 +5,6 @@ import net.minecraft.block.BlockState;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 
