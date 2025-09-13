1c675e406833532ba9f0c4c876f388889d176326
Caranouga <malonepayet@gmail.com>
2025-09-14 00:33:56 +0200

    Added current tech level next to the bar

--- src/main/java/fr/caranouga/expeditech/client/screens/TechLevelScreen.java
+++ src/main/java/fr/caranouga/expeditech/client/screens/TechLevelScreen.java
@@ -14,6 +14,10 @@ import static fr.caranouga.expeditech.common.utils.StringUtils.modLocation;
 public class TechLevelScreen {
     private static final ResourceLocation TEXTURE = modLocation("textures/gui/widgets/progress_bar_bg.png");
 
+    private static final int progressBarX = 1;
+    private static final int progressBarY = 1;
+    private static final ProgressBarWidget progressBarWidget = new ProgressBarWidget(progressBarX, progressBarY, 0xFFEE00);
+
     public static void render(MatrixStack pMatrixStack) {
         Minecraft mc = Minecraft.getInstance();
         PlayerEntity player = mc.player;
@@ -27,8 +31,13 @@ public class TechLevelScreen {
 
             mc.getTextureManager().bind(TEXTURE);
 
-            AbstractGui.blit(pMatrixStack, 0, 0, 0, 0, 82, 8, 82, 8);
-            new ProgressBarWidget(1, 1, 0xFFEE00).render(pMatrixStack, 0, 0, xpBarWidth);
+            int bgWidth = ProgressBarWidget.WIDTH + 2 * progressBarX;
+            int bgHeight = ProgressBarWidget.HEIGHT + 2 * progressBarY;
+
+            AbstractGui.blit(pMatrixStack, 0, 0, 0, 0, bgWidth, bgHeight, bgWidth, bgHeight);
+            progressBarWidget.render(pMatrixStack, 0, 0, xpBarWidth);
+
+            mc.font.drawShadow(pMatrixStack, techLevel.getTechLevel() + "", bgWidth + 1, progressBarY, 0xFFFFFF);
         });
     }
 
